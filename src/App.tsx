ff2961f61f69748dfa677f7489f097cf2b1744e8
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-02-27 11:30:36 +0900

    chore: react-query 기본 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>main</div>
+    </QueryClientProvider>
   );
 }
 
