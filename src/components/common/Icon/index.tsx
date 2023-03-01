cd35b712eaf1cfda76cda1b2e2f3ba54a5c23306
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-01 16:21:56 +0900

    refactor: 아이콘 컴포넌트 수정

--- src/components/common/Icon/index.tsx
+++ src/components/common/Icon/index.tsx
@@ -4,25 +4,37 @@ import Icons from "../../../constants/icon";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 interface IconProps {
-  className: string;
+  className?: string;
+  iconName: string;
   size?: string;
   color?: string;
   border?: number;
 }
 
+const Wrapper = styled.div`
+  background-color: transparent;
+`;
 const StyledIcon = styled.i<{ border: number }>`
   ${({ border }) => `
    -webkit-text-stroke: ${border}px;
 `}
 `;
 
-function Icon({ className, size = "1.5rem", color, border = 0.1 }: IconProps) {
+function Icon({
+  className,
+  iconName,
+  size = "1.5rem",
+  color,
+  border = 0.1,
+}: IconProps) {
   return (
-    <StyledIcon
-      className={Icons[className]}
-      style={{ fontSize: size, color }}
-      border={border}
-    />
+    <Wrapper className={className}>
+      <StyledIcon
+        className={Icons[iconName]}
+        style={{ fontSize: size, color }}
+        border={border}
+      />
+    </Wrapper>
   );
 }
 
