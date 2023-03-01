0b64fb6ebaf6d97ec859c6cf7cf45d8e393b65a2
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-01 14:20:58 +0900

    feat: Icon 컴포넌트 스토리 작성

---
+++ src/components/common/Icon/index.stories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { withKnobs, text, boolean } from "@storybook/addon-knobs";
+import Icon from "./index";
+import * as colors from "../../../constants/color";
+
+export default {
+  title: "components|basic/Icon", // 스토리북에서 보여질 그룹과 경로를 명시
+  component: Icon, // 어떤 컴포넌트를 문서화 할지 명시
+  decorators: [withKnobs], // 애드온 적용
+};
+
+export function Check() {
+  return <Icon className="checkFill" color={colors.BLUE} />;
+}
+export function Book() {
+  return <Icon className="book" color={colors.GREY[600]} />;
+}
+export function Pig() {
+  return <Icon className="pig" color={colors.GREY[600]} />;
+}
+export function Chart() {
+  return <Icon className="chart" color={colors.GREY[600]} />;
+}
+export function Bank() {
+  return <Icon className="bank" color={colors.GREY[600]} />;
+}
+export function PersonGear() {
+  return <Icon className="personGear" color={colors.GREY[600]} />;
+}
