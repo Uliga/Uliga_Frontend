abbd4f5c41bfd646bf926989693b71f21c5b7225
miin-hyukkk <ham9893@naver.com>
2023-03-02 18:14:39 +0900

    chore: 안쓰는 import 정리

--- src/components/common/IconButton/index.stories.tsx
+++ src/components/common/IconButton/index.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { withKnobs, text, boolean } from "@storybook/addon-knobs";
 import IconButton from "./index";
-import * as colors from "../../../constants/color";
 
 export default {
   title: "components|basic/IconButton", // 스토리북에서 보여질 그룹과 경로를 명시
