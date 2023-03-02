23f916925686e6807fc4b6ad88d2ce519fbc7039
miin-hyukkk <ham9893@naver.com>
2023-03-02 12:49:30 +0900

    feat: 아이콘버튼 스토리 추가

--- src/components/common/IconButton/index.stories.tsx
+++ src/components/common/IconButton/index.stories.tsx
@@ -8,3 +8,42 @@ export default {
   component: IconButton, // 어떤 컴포넌트를 문서화 할지 명시
   decorators: [withKnobs], // 애드온 적용
 };
+
+export function Normal() {
+  return (
+    <IconButton
+      title="가계부"
+      theme="normal"
+      iconName="book"
+      widthSize={6.0625}
+      heightSize={1.7}
+      iconSize="1.1rem"
+    />
+  );
+}
+export function Primary() {
+  return (
+    <IconButton title="구성원 추가하기" theme="primary" iconName="personGear" />
+  );
+}
+export function Secondary() {
+  return (
+    <IconButton
+      title="구성원 추가하기"
+      theme="secondary"
+      iconName="personGear"
+    />
+  );
+}
+export function Teritiary() {
+  return (
+    <IconButton
+      title="윤채현님의 가계부"
+      theme="tertiary"
+      iconName="checkFill"
+    />
+  );
+}
+export function OnlyBook() {
+  return <IconButton iconOnly iconName="book" />;
+}
