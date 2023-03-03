32333fac67842a6d3ee1411e5549d207e04ab1d3
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-03 15:32:32 +0900

    feat: axios interceptors 추가

---
+++ src/api/index.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import API from "./config";
+
+axios.defaults.baseURL = API.BASE_URL;
+axios.defaults.withCredentials = true;
+
+const authorizationClient = axios.create({
+  baseURL: API.BASE_URL,
+  withCredentials: true,
+});
+
+authorizationClient.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    console.error("[Axios]", error);
+    return Promise.reject(error);
+  },
+);
+
+const unAuthorizationClient = axios.create({
+  baseURL: API.BASE_URL,
+  withCredentials: true,
+});
+
+unAuthorizationClient.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    console.error("[Axios]", error);
+    return Promise.reject(error);
+  },
+);
+
+export { authorizationClient, unAuthorizationClient };
