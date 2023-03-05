58bf1d73614f794064d0518391b61877ed6a8307
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-05 12:45:06 +0900

    feat: 레이아웃 컴포넌트 구현

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import SideBar from "./SideBar";
+
+const Full = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  flex-direction: column;
+`;
+
+const Inner = styled.div`
+  width: 140.8rem;
+  padding-top: 5.5rem;
+  background-color: white;
+`;
+export default function MainLayout() {
+  return (
+    <Full>
+      <Header />
+      <Inner>
+        <SideBar />
+        <Outlet />
+      </Inner>
+    </Full>
+  );
+}
