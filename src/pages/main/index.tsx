ee4d705893623bc5fc52589cb4da42a6c6d81964
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-06 20:13:36 +0900

    chore: 메인 페이지에 내 정보 조회 api 추가

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { loadMe } from "../../api/user";
+import LoadingBar from "../../components/common/LoadingBar";
 
 export default function Main() {
-  return <>Main Page</>;
+  const { isLoading, data } = useQuery(["me"], loadMe);
+  if (isLoading) return <LoadingBar type={8} />;
+  return <div>{data.email}</div>;
 }
