e24dd8a4f78f4cab221f5d407d56789fe4e45014
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-06 14:23:19 +0900

    feat: useLogin hook 구현

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -3,28 +3,29 @@ import { useMutation } from "@tanstack/react-query";
 import useInput from "./useInput";
 import REGEX from "../constants/regex";
 import { authLogin } from "../api/auth";
+import toastMsg from "../components/common/Toast";
 
 export default function useLogin() {
   const [email, onChangeEmail] = useInput("");
   const [password, onChangePassword] = useInput("");
   const [isValidate, setIsValidate] = useState(true);
-
   useEffect(() => {
     setIsValidate(REGEX.ID.test(email));
   }, [email]);
 
   const mutateLogin = useMutation(["login"], authLogin, {
     onSuccess: () => {
-      alert("성공");
+      toastMsg("로그인 성공");
     },
     onError: ({
       response: {
-        data: { message },
+        data: { errorCode, message },
       },
     }) => {
-      alert(message);
+      toastMsg(`${errorCode} / ${message}`);
     },
   });
+
   return {
     isValidate,
     email,
