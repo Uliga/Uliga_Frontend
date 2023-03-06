79c3eabd8a4ff9f0621c91d2d8346f9307a9ea5b
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-06 20:10:18 +0900

    feat: 이메일 중복 확인 api 작성

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -10,3 +10,10 @@ export const authSignup = async (userData: object) => {
   const { data } = await unAuthorizationClient.post(API.SIGNUP, userData);
   return data;
 };
+
+export const checkEmail = async (email: string) => {
+  const { data } = await unAuthorizationClient.get(
+    `${API.CHECK_EMAIL}${email}`,
+  );
+  return data;
+};
