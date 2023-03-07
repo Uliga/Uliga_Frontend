3d05fa3f2093862b8b69286d71aebf652b21d46b
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-07 13:38:24 +0900

    feat: useValidate hook 추가

---
+++ src/hooks/useValidate.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+import useInput from "./useInput";
+
+interface ValidateProps {
+  validator: Function;
+  initState?: string;
+}
+
+const useValidate = ({ validator, initState = "" }: ValidateProps) => {
+  const [value, onChangeValue] = useInput(initState);
+  const [isValidateValue, setIsValidateValue] = useState(false);
+
+  useEffect(() => {
+    if (value) {
+      setIsValidateValue(validator(value));
+    }
+  }, [value, validator]);
+
+  return [value, onChangeValue, isValidateValue, setIsValidateValue] as const;
+};
+
+export default useValidate;
