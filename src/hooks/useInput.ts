67e17a6cb7088468403a95967a652acba50b52ff
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-07 13:38:56 +0900

    refactor: useValidate로 로그인 코드 리팩토링

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -2,10 +2,13 @@ import { useState, useCallback } from "react";
 
 const useInput = (initialValue: any) => {
   const [value, setValue] = useState(initialValue);
-  const handler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  }, []);
-  return [value, handler, setValue];
+  const onChagneValue = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value);
+    },
+    [],
+  );
+  return [value, onChagneValue, setValue];
 };
 
 export default useInput;
