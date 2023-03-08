100bf6e0d5446462d153e06a132a0804c27e592d
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-08 21:27:53 +0900

    feat: Modal 컴포넌트 구현

---
+++ src/components/common/Modal/index.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+import COLORS from "../../../constants/color";
+import IconButton from "../IconButton";
+
+const ModalBackground = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  display: flex;
+  z-index: 999;
+`;
+
+const ModalBox = styled.div`
+  position: fixed;
+  border-radius: 1rem;
+  width: 50rem;
+  background-color: white;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Button = styled(IconButton)`
+  position: absolute;
+  right: 3rem;
+  top: 2rem;
+`;
+
+interface ModalProps {
+  children: React.ReactNode;
+  closeModal: () => void;
+}
+export default function Modal({ children, closeModal }: ModalProps) {
+  return (
+    <ModalBackground onClick={closeModal}>
+      <ModalBox onClick={e => e.stopPropagation()}>
+        {children}
+        <Button
+          iconOnly
+          iconName="close"
+          iconSize="2.5rem"
+          color={COLORS.GREY[300]}
+          border={0.6}
+          onClick={closeModal}
+        />
+      </ModalBox>
+    </ModalBackground>
+  );
+}
