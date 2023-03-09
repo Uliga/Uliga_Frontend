720524f1a20b20767c50f0f45cbb1625d2136076
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-09 16:39:14 +0900

    fix: Layout 폴더명 conflict 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MainLayout from "./components/layout/MainLayout";
-import DefaultLayout from "./components/layout/DefaultLayout";
+import MainLayout from "./components/Layout/MainLayout";
+import DefaultLayout from "./components/Layout/DefaultLayout";
 import Login from "./pages/login";
 import Main from "./pages/main";
 import PATH from "./constants/path";
