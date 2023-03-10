a21d5ac51d0fdf8120f927c059845d83934deec6
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-10 20:59:39 +0900

    feat: bookinfo interface 추가

---
+++ src/interfaces/book.ts
@@ -0,0 +1,17 @@
+export interface IBookInfo {
+  categories: object[];
+  info: BookInfoTypes;
+  members: object[];
+  numberOfMember: {
+    count: number;
+  };
+}
+
+export interface BookInfoTypes {
+  accountBookAuthority: "ADMIN" | "USER";
+  accountBookId: number;
+  accountBookName: string;
+  getNotification: boolean;
+  isPrivate: boolean;
+  relationShip: string;
+}
