bd0d8b62a39eef86c1dccf00562717b84e44cf7d
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-11 20:09:56 +0900

    feat: context 관련 atom 추가

--- src/stores/atoms/user.ts
+++ src/stores/atoms/user.ts
@@ -8,7 +8,7 @@ const meAtom = atom<IUser>({
       id: "",
       avatarUrl: "",
       username: "",
-      nickname: "",
+      nickName: "",
       applicationPassword: "",
       email: "",
     },
