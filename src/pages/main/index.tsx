868c012d877729ac4631bbdf6b6794cafc4a067f
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-11 20:09:56 +0900

    feat: context 관련 atom 추가

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { useQuery } from "@tanstack/react-query";
+import { useRecoilState } from "recoil";
 import { loadMe } from "../../api/user";
 import LoadingBar from "../../components/LoadingBar";
 import Button from "../../components/Button";
 import Modal from "../../components/Modal";
 import Create from "../create";
 import QUERYKEYS from "../../constants/querykey";
+import BookNav from "../../components/BookNav";
+import { createModalAtom } from "../../stores/atoms/context";
 
 const WriteButton = styled(Button)`
   font-size: 1.4rem;
@@ -16,29 +19,31 @@ const WriteButton = styled(Button)`
   padding: 1.2rem 2.5rem;
 `;
 
+const Container = styled.div`
+  width: 100%;
+  padding: 3rem;
+`;
+
 export default function Main() {
   const { isLoading, data } = useQuery([QUERYKEYS.LOAD_ME], loadMe);
-  const [openModal, setModal] = useState(true);
-  const closeModal = () => {
-    setModal(false);
-  };
+  console.log(data);
+  const [createModalOpen, setCreateModalOpen] = useRecoilState(createModalAtom);
+
   if (isLoading) return <LoadingBar type={8} />;
 
   return (
-    <div>
-      <WriteButton
-        theme="primary"
-        title="가계부 작성하기"
-        onClick={() => {
-          setModal(true);
-        }}
-      />
-      {openModal && (
-        <Modal closeModal={closeModal}>
+    <Container>
+      <BookNav />
+      <WriteButton theme="primary" title="가계부 작성하기" />
+      {createModalOpen && (
+        <Modal
+          closeModal={() => {
+            setCreateModalOpen(false);
+          }}
+        >
           <Create />
         </Modal>
       )}
-      {data.memberInfo.email}
-    </div>
+    </Container>
   );
 }
--- src/stores/atoms/book.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-const selectedBookAtom = atom<number>({
-  key: "selectedBook",
-  default: 0,
-});
-
-export default selectedBookAtom;
