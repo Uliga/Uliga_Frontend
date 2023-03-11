2f4071a1e2e2320596d250b665062da03fc0d17c
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-11 19:57:14 +0900

    feat: 가계부 리스트 nav bar 구현

---
+++ src/components/BookNav/index.styles.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import Button from "../Button";
+import IconButton from "../IconButton";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+const CheckedButton = styled(IconButton)`
+  border-radius: 30rem;
+  font-size: 1.5rem;
+  padding: 1rem 2.5rem;
+`;
+
+const UnCheckedButton = styled(Button)`
+  border-radius: 30rem;
+  font-size: 1.5rem;
+  padding: 1rem 2.5rem;
+`;
+
+const ETCButton = styled(IconButton)<{}>`
+  border-radius: 30rem;
+  padding: 1rem 1.2rem;
+`;
+
+const Nav = styled.div`
+  display: flex;
+  max-width: 96rem;
+  flex-wrap: wrap;
+  gap: 1rem;
+  position: relative;
+`;
+
+export { Container, CheckedButton, UnCheckedButton, ETCButton, Nav };
---
+++ src/components/BookNav/index.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import COLORS from "../../constants/color";
+import QUERYKEYS from "../../constants/querykey";
+import { loadBookList } from "../../api/book";
+import { IBookList } from "../../interfaces/book";
+import BottomModal from "./BottomModal";
+import * as S from "./index.styles";
+import { bottomModalAtom, createModalAtom } from "../../stores/atoms/context";
+
+export default function BookNav() {
+  const { bookId } = useParams();
+  const { data } = useQuery<IBookList>(
+    [QUERYKEYS.LOAD_BOOK_LIST],
+    loadBookList,
+  );
+  const [bottomModalOpen, setBottomModalOpen] = useRecoilState(bottomModalAtom);
+  const [createModalOpen, setCreateModalOpen] = useRecoilState(createModalAtom);
+  if (!data) return null;
+
+  return (
+    <S.Container>
+      <S.Nav>
+        {data.accountBooks.length <= 4
+          ? data.accountBooks.map(book =>
+              bookId && book.accountBookId ? (
+                <S.CheckedButton
+                  title={book.accountBookName}
+                  iconName="checkFill"
+                  theme="tertiary"
+                  color={COLORS.BLUE}
+                />
+              ) : (
+                <S.UnCheckedButton title={book.accountBookName} theme="basic" />
+              ),
+            )
+          : data.accountBooks
+              .slice(0, 4)
+              .map(book =>
+                bookId && book.accountBookId ? (
+                  <S.CheckedButton
+                    title={book.accountBookName}
+                    iconName="checkFill"
+                    theme="tertiary"
+                    color={COLORS.BLUE}
+                  />
+                ) : (
+                  <S.UnCheckedButton
+                    title={book.accountBookName}
+                    theme="tertiary"
+                  />
+                ),
+              )}
+        <S.ETCButton
+          iconName="plus"
+          theme={createModalOpen ? "tertiary" : "basic"}
+          onClick={() => {
+            setCreateModalOpen(true);
+          }}
+        />
+        <S.ETCButton
+          iconName="dots"
+          theme={bottomModalOpen ? "tertiary" : "basic"}
+          onClick={() => {
+            setBottomModalOpen(!bottomModalOpen);
+          }}
+        />
+        {bottomModalOpen && <BottomModal accountBooks={data.accountBooks} />}
+      </S.Nav>
+    </S.Container>
+  );
+}
