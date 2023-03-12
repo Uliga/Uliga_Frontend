9e764459abd7044fc9271cc8ca5b167925ad6d6d
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-12 14:41:46 +0900

    chore: 색상, 아이콘, 쿼리키 상수 추가

--- src/constants/querykey.ts
+++ src/constants/querykey.ts
@@ -2,6 +2,7 @@ const QUERYKEYS = {
   LOAD_ME: "loadMe",
   LOAD_BOOK_INFO: "loadBookInfo",
   LOAD_BOOK_LIST: "loadBookList",
+  LOAD_MONTH_ASSET: "loadMonthAsset",
 } as const;
 
 export default QUERYKEYS;
