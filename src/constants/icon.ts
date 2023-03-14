41d21eb34b80929d3f9ef75c52da57613968cf80
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-14 21:47:57 +0900

    chore: 아이콘 추가

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -17,6 +17,8 @@ const ICONS: IconProps = {
   notice: "bi bi-patch-exclamation-fill",
   check: "bi bi-check",
   circleCheck: "bi bi-plus-circle",
+  message: "bi bi-envelope",
+  messageOpen: "bi bi-envelope-open-fill",
 };
 
 export default ICONS;
