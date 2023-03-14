5cfd93be701b63b380bfc09067a2e64dc86b18da
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-14 16:30:44 +0900

    feat: 가계부 일괄 작성 기능 추가

---
+++ src/pages/write/SelectContainer.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { SelectContainer } from "./index.styles";
+
+interface Props {
+  options: Array<{ value: string | undefined; label: string }>;
+  value?: any;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+// eslint-disable-next-line react/function-component-definition
+const SelectWindow: React.FC<Props> = ({ options, value, onChange }) => {
+  return (
+    <SelectContainer value={value} onChange={onChange}>
+      {options.map(option => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </SelectContainer>
+  );
+};
+
+export default SelectWindow;
