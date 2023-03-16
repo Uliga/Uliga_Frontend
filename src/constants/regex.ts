75a33e6884ec1deb6b5421d6d445be67bdc44275
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-17 00:56:03 +0900

    chore: 화폐 단위 정규식 추가

--- src/constants/regex.ts
+++ src/constants/regex.ts
@@ -2,6 +2,7 @@ const REGEX = {
   ID: /^[0-9a-zA-Z가-힣]([-_.]?[0-9a-zA-Z가-힣])*@[0-9a-zA-Z가-힣]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
   PASSWORD: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,19}$/,
   NICKNAME: /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{2,19}$/,
+  MONEY: /\B(?=(\d{3})+(?!\d))/g,
 };
 
 export default REGEX;
