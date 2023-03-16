680ddfad0d6371d2348442373df36a2b8e2f8d7e
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-16 19:19:57 +0900

    chore: 가계부 관련 상수 추가

--- src/constants/querykey.ts
+++ src/constants/querykey.ts
@@ -3,6 +3,7 @@ const QUERYKEYS = {
   LOAD_BOOK_INFO: "loadBookInfo",
   LOAD_BOOK_LIST: "loadBookList",
   LOAD_MONTH_ASSET: "loadMonthAsset",
+  LOAD_MONTH_ITEM: "loadMonthItem",
 } as const;
 
 export default QUERYKEYS;
