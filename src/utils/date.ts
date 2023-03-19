eb20ba4477195a11f4533951c6338c1ac46eb1c3
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-03-19 23:24:22 +0900

    feat: 현재 날짜, 시간 가져오는 util 추가

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -11,3 +11,19 @@ export default function changeDateUnit(day: any) {
 
   return date;
 }
+
+export function changeFullDateUnit(day: any) {
+  const time = day.toLocaleTimeString().split(" ")[1];
+
+  const date = `${day.getFullYear()}-${
+    day.getMonth() + 1 < 10
+      ? `0${(day.getMonth() + 1).toString()}`
+      : (day.getMonth() + 1).toString()
+  }-${
+    day.getDate() < 10
+      ? `0${day.getDate().toString()}`
+      : day.getDate().toString()
+  } ${time}`;
+
+  return date;
+}
