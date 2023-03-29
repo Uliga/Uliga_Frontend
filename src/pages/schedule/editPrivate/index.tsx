41c2acc3eb22a995497168c83b1cb261cd3f1dac
miin-hyukkk <ham9893@naver.com>
2023-03-29 16:18:04 +0900

    chore: 콘솔 제거

--- src/pages/schedule/editPrivate/index.tsx
+++ src/pages/schedule/editPrivate/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-// import { text } from "@storybook/addon-knobs";
 import * as S from "./index.styles";
 import getMoneyUnit from "../../../utils/money";
 import { IScheduleDetail } from "../../../interfaces/schedule";
