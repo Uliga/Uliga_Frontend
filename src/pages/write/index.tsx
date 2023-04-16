6058bf61c56e16f684b5bcc84b88fe5c0a2e8784
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-04-16 16:01:17 +0900

    feat: 일일 가계부 내역 작성 공유 가계부 추가 구현

--- src/pages/write/index.tsx
+++ src/pages/write/index.tsx
@@ -48,6 +48,7 @@ export default function Write() {
                     inputList={inputList}
                     setInputList={setInputList}
                     listIdx={listIdx}
+                    isMultiple
                   />
                 </Modal>
               )}
