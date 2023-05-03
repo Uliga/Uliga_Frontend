dac41fa9c8f5a812d230d2674fa59b2a4fec736a
miin-hyukkk <ham9893@naver.com>
2023-05-03 12:34:36 +0900

    feat: 태블릿 사이드바 핸들링 구현

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import SideBar from "./SideBar";
 import COLORS from "../../constants/color";
+import media from "../../styles/media";
 
 const Full = styled.div`
   width: 100%;
@@ -20,6 +21,9 @@ const Inner = styled.div`
   background-color: white;
   height: 100vh;
   border-right: 0.1rem solid ${COLORS.GREY[200]};
+  ${media.medium} {
+    width: 123.8rem;
+  }
 `;
 
 const Container = styled.div`
@@ -28,17 +32,58 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   position: relative;
+  ${media.medium} {
+    left: 0;
+  }
 `;
-
 export default function MainLayout() {
+  const [showSideBar, setShowSideBar] = useState(false);
+
+  const toggleSideBar = () => {
+    setShowSideBar(!showSideBar);
+  };
+
+  const handleCloseSideBar = () => {
+    setShowSideBar(false);
+  };
+
+  const handleOutletClick = () => {
+    if (
+      window.matchMedia("(min-width: 768px) and (max-width: 1024px)").matches
+    ) {
+      setShowSideBar(false);
+    }
+  };
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(
+      "(min-width: 768px) and (max-width: 1024px)",
+    );
+    setShowSideBar(!mediaQuery.matches);
+    const handler = () => setShowSideBar(!mediaQuery.matches);
+    mediaQuery.addEventListener("change", handler);
+    return () => {
+      mediaQuery.removeEventListener("change", handler);
+    };
+  }, []);
+
+  console.log("showSideBar", showSideBar);
   return (
     <Full>
-      <Header />
-      <Inner>
-        <SideBar />
-        <Container>
-          <Outlet />
-        </Container>
+      <Header onToggleSideBar={toggleSideBar} />
+      <Inner onClick={handleOutletClick}>
+        {showSideBar && (
+          <>
+            <SideBar onClose={handleCloseSideBar} />
+            <Container onClick={handleOutletClick}>
+              <Outlet />
+            </Container>
+          </>
+        )}
+        {!showSideBar && (
+          <Container onClick={handleOutletClick}>
+            <Outlet />
+          </Container>
+        )}
       </Inner>
     </Full>
   );
