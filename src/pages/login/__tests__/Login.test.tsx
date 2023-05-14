a078035087f134eb4644439b27a45c0eaf320693
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-05-14 14:18:40 +0900

    test: mutateLogin 함수 호출 테스트 코드 추가

--- src/pages/login/__tests__/Login.test.tsx
+++ src/pages/login/__tests__/Login.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { BrowserRouter as Router } from "react-router-dom";
 import "@testing-library/jest-dom";
@@ -33,6 +33,13 @@ function renderLogin() {
   const LoginButton = () =>
     testUtils.getByText("로그인", { selector: "button" });
 
+  const onClickLoginButton = () => {
+    userEvent.click(LoginButton());
+    const email = (Email() as HTMLInputElement).value;
+    const password = (Password() as HTMLInputElement).value;
+    mutateLogin({ email, password });
+  };
+
   return {
     Email,
     Password,
@@ -41,6 +48,7 @@ function renderLogin() {
     typeEmail,
     typePassword,
     LoginButton,
+    onClickLoginButton,
   };
 }
 
@@ -55,5 +63,27 @@ describe("<Login />", () => {
     expect(Email()).toBeInTheDocument();
     expect(Password()).toBeInTheDocument();
     expect(LoginButton()).toBeInTheDocument();
+  });
+  it("유저 로그인 정보로 mutateLogin 함수를 호출한다.", async () => {
+    const {
+      Email,
+      Password,
+      typeEmail,
+      typePassword,
+      onClickLoginButton,
+      mutateLogin,
+    } = renderLogin();
+
+    typeEmail("dbscogus4467@naver.com");
+    typePassword("test123!");
+    onClickLoginButton();
+
+    // 이메일과 비밀번호 필드의 값이 예상한 값과 일치하는지를 검증
+    expect(Email()).toContainHTML("dbscogus4467@naver.com");
+    expect(Password()).toContainHTML("test123!");
+    expect(mutateLogin).toHaveBeenCalledWith({
+      email: "dbscogus4467@naver.com",
+      password: "test123!",
+    });
   });
 });
