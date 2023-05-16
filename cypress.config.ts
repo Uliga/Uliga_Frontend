e3a720800d5bfdb9c18870b3436c1bb33c4b86d6
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-05-16 20:17:19 +0900

    config: cypress-react-selector 설치

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   env: {
     apiHost: process.env.REACT_APP_BASE_URL,
+    baseUrl: "http://localhost:3000",
+    "cypress-react-selector": {
+      root: "#root",
+    },
   },
   e2e: {
     setupNodeEvents(on, config) {
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,5 +1,6 @@
 // Import commands.js using ES2015 syntax:
 import "./commands";
+import "cypress-react-selector";
 
 export {};
 declare global {
