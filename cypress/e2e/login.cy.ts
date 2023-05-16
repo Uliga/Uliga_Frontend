41987006fe268fb5f8b2d9e56d66f16272e0fd3b
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-05-16 16:19:04 +0900

    test: 로그인 실패했을 경우, 아이디 비밀번호 유효성 검사 테스트 코드 작성

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -3,20 +3,44 @@ import API from "../../src/api/config";
 describe("login e2e test", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/login");
+  });
+  it("유효한 아이디와 비밀번호로 로그인에 성공한다.", () => {
     cy.intercept(
       { method: "POST", url: API.LOGIN },
       { fixture: "loginResult.json" },
-    ).as("loginResult");
-  });
-  it("유효한 아이디와 비밀번호로 로그인에 성공한다.", () => {
+    ).as("loginSuccess");
+
     cy.contains("이메일 주소").parent().type("dbscogus4467@naver.com");
     cy.contains("비밀번호").parent().type("rhdqngkwk7!");
     cy.get("button").click();
     cy.wait(500);
-    cy.wait("@loginResult").then(interception => {
-      expect(interception?.response?.statusCode).to.equal(200);
+
+    cy.wait("@loginSuccess")
+      .its("response.body")
+      .then(data => {
+        expect(data.memberInfo.privateAccountBookId).to.equal(52);
+        cy.location("pathname", { timeout: 5000 }).should(
+          "include",
+          `main/${data.memberInfo.privateAccountBookId}`,
+        );
+      });
+  });
+  it("유효하지 않은 아이디와 비밀번호로 로그인에 실패한다.", () => {
+    cy.intercept(
+      { method: "POST", url: API.LOGIN },
+      { statusCode: 401, body: { error: "Login failed" } },
+    ).as("loginFailure");
+    cy.contains("이메일 주소").parent().type("dbscogus4467@naver.com");
+    cy.contains("비밀번호").parent().type("wrong password");
+    cy.get("button").click();
+    cy.wait(500);
+
+    cy.wait("@loginFailure").then(interception => {
+      expect(interception?.response?.statusCode).to.equal(401);
     });
   });
-  it("유효하지 않은 아이디와 비밀번호로 로그인에 실패한다.", () => {});
-  it("아이디와 비밀번호 필드에 아무값도 입력하지 않는다.", () => {});
+  it("아이디와 비밀번호 필드에 올바르지 않은 값을 입력하여 버튼이 disabled 상태가 된다.", () => {
+    cy.contains("이메일 주소").parent().type("dbscogus4467navercom");
+    cy.get("button").should("be.disabled");
+  });
 });
