ccd3e333f3f0c9b39c308d571e1ca4574fb35394
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-05-16 18:20:10 +0900

    config: 테스트 코드에서 실제 유저랑 통신하기 위한 cypress commands 추가

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  env: {
+    apiHost: process.env.REACT_APP_BASE_URL,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,18 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+import API from "../../src/api/config";
+
+Cypress.Commands.add("login", () => {
+  cy.request({
+    url: `${Cypress.env("apiHost")}${API.LOGIN}`,
+    method: "POST",
+    body: { email: Cypress.env("email"), password: Cypress.env("password") },
+  })
+    .its("body")
+    .then(data => {
+      localStorage.setItem(
+        "privateAccountBookId",
+        `${data.memberInfo.privateAccountBookId}`,
+      );
+      localStorage.setItem("accessToken", `${data.tokenInfo.accessToken}`);
+      localStorage.setItem("created", "true");
+    });
+});
