ce5aacdd05f7338593f5808716cbe5ec890fbc1e
miin-hyukkk <ham9893@naver.com>
2023-05-18 20:13:04 +0900

    test: 인증번호 불일치로 회원가입 실패시 test 구현

--- cypress/e2e/1-landing-page/signup.spec.cy.ts
+++ cypress/e2e/1-landing-page/signup.spec.cy.ts
@@ -143,4 +143,64 @@ describe("회원가입", () => {
     cy.contains("계정 만들기").should("be.disabled");
     // '확인' 버튼을 포함한 요소 선택
   });
+  it("인증번호 불일치로 회원가입 실패", () => {
+    cy.intercept(
+      {
+        method: "POST",
+        url: API.EMAIL_SEND,
+      },
+      {
+        email: "test@email.com",
+        success: true,
+      },
+    );
+    cy.contains("인증").click();
+    cy.contains("인증번호 입력").parent().type(verificationCode);
+
+    cy.intercept(
+      {
+        method: "POST",
+        url: API.CODE,
+      },
+      {
+        matches: false,
+      },
+    );
+    cy.contains("인증 완료").click();
+    cy.contains("인증 완료").should("not.be.disabled");
+    cy.contains("비밀번호").parent().type(password);
+    cy.contains("비밀번호 확인").parent().type(password);
+    cy.contains("이름").parent().type(name);
+    cy.contains("닉네임").parent().type(nickname);
+    cy.intercept(
+      {
+        method: "GET",
+        url: `${API.NICK_DUPLICATE}${nickname}`,
+      },
+      {
+        exists: false,
+      },
+    );
+    cy.contains("닉네임") // '닉네임 확인' 라벨을 포함한 요소 선택
+      .parent()
+      .parent() // 다음 형제 요소 선택 (오른쪽에 위치한 요소)
+      .contains("확인") // '확인' 버튼을 포함한 요소 선택
+      .click();
+    cy.get('input[type="checkbox"]').click();
+    cy.intercept(
+      {
+        method: "POST",
+        url: `${API.SIGNUP}`,
+      },
+      {
+        email: "testuser@example.com",
+        password,
+        nickName: nickname,
+        userName: name,
+        applicationPassword: "1234",
+      },
+    );
+    cy.contains("계정 만들기").should("be.disabled");
+    // '확인' 버튼을 포함한 요소 선택
+  });
 });
