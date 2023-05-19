3761761aa79718f7a85b66e440d7d357ff71fe83
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-05-19 18:02:20 +0900

    fix: 환경변수 설정 변경

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,7 +2,7 @@ import API from "../../src/api/config";
 
 Cypress.Commands.add("login", () => {
   cy.request({
-    url: `${process.env.REACT_APP_BASE_URL}${API.LOGIN}`,
+    url: `${Cypress.env("CYPRESS_API_HOST")}${API.LOGIN}`,
     method: "POST",
     body: { email: "dbscogus4467@naver.com", password: "rhdqngkwk7!" },
   })
