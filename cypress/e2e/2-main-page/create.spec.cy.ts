99903f1372db25aa6ef9c7e8a0161cfc6c8891e9
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-05-20 23:06:57 +0900

    test: 가계부 생성 페이지 cy.viewport 설정

--- cypress/e2e/2-main-page/create.spec.cy.ts
+++ cypress/e2e/2-main-page/create.spec.cy.ts
@@ -2,25 +2,25 @@ import API from "../../../src/api/config";
 
 describe("create accountbook e2e test", () => {
   beforeEach(() => {
-    // cy.intercept(
-    //   {
-    //     method: "GET",
-    //     url: `${API.CHECK_EMAIL}dbscogus44@gmail.com`,
-    //   },
-    //   { exists: true },
-    // ).as("checkEmail");
-    // cy.intercept(
-    //   {
-    //     method: "POST",
-    //     url: `${API.ACCOUNT_BOOK}`,
-    //   },
-    //   {
-    //     id: 0,
-    //     name: "accountBookName",
-    //     isPrivate: true,
-    //     relationShip: "relationship",
-    //   },
-    // ).as("createAccountBook");
+    cy.intercept(
+      {
+        method: "GET",
+        url: `${API.CHECK_EMAIL}dbscogus44@gmail.com`,
+      },
+      { exists: true },
+    ).as("checkEmail");
+    cy.intercept(
+      {
+        method: "POST",
+        url: `${API.ACCOUNT_BOOK}`,
+      },
+      {
+        id: 0,
+        name: "accountBookName",
+        isPrivate: true,
+        relationShip: "relationship",
+      },
+    ).as("createAccountBook");
     cy.login().then(() => {
       cy.visit(
         `http://localhost:3000/main/${localStorage.getItem(
@@ -30,6 +30,7 @@ describe("create accountbook e2e test", () => {
     });
   });
   it("가계부 생성 버튼이 뜨는지 확인한다.", () => {
+    cy.viewport(1920, 1080);
     cy.get('[data-cy="create-book-button"]').should("be.visible");
   });
   it("새로운 가계부를 생성하는 기능이 잘 작동하는지 확인한다.", () => {
@@ -51,7 +52,7 @@ describe("create accountbook e2e test", () => {
         "추가된 카테고리",
       );
       cy.contains("공유 가계부 만들기").click();
-      // cy.wait("@createAccountBook");
+      cy.wait("@createAccountBook");
     });
   });
 });
