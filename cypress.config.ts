a1bf78da8153ad6952628ea78e1e814149127e61
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-05-20 20:03:49 +0900

    test: cypress, github action ci test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   env: {
-    apiHost: process.env.REACT_APP_BASE_URL,
+    CYPRESS_API_HOST: process.env.REACT_APP_BASE_URL,
     baseUrl: "http://localhost:3000",
     "cypress-react-selector": {
       root: "#root",
