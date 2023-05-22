6f0e6ea1fd846619d74b18a0aa9c7d2523f332fd
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-05-22 17:40:56 +0900

    test: 가계부 내역을 공유 가계부에 추가하는 시나리오 테스트 코드 작성

--- cypress/e2e/3-write-page/write.spec.cy.ts
+++ cypress/e2e/3-write-page/write.spec.cy.ts
@@ -56,7 +56,22 @@ describe("write page e2e test", () => {
     cy.wait("@uploadBook");
   });
 
-  it("가계부에 지출 내역을 작성하고 동일한 내용을 공유가계부에 추가한다.", () => {});
+  it("가계부에 지출 내역을 작성하고 동일한 내용을 공유가계부에 추가한다.", () => {
+    cy.get('[data-cy="add-otherBook-button"]').click();
+    cy.get('[data-cy="other-account-book"]').find("button").click();
+    cy.get('[data-cy="modal-submit-button"]').click();
+    cy.get('[data-cy="write-input-container"]').within(() => {
+      cy.get("select").eq(0).select(2);
+      cy.get('input[type="date"]').type("2023-05-22");
+      cy.get("select").eq(1).select(2);
+      cy.get("select").eq(2).select(2);
+      cy.get('input[type="text"]').eq(0).type("거래처 테스트");
+      cy.get('input[type="number"]').type("3000");
+      cy.get('input[type="text"]').eq(1).type("메모 테스트");
+    });
+    cy.get('[data-cy="write-submit-button"]').click();
+    cy.wait("@uploadBook");
+  });
 
   it("가계부에 2개 이상의 내역을 작성한다.", () => {
     cy.get('[data-cy="write-plus-button"]').click();
