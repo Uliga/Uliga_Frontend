531f84b63aa89ee1fd7c3cd24ebdbe97c480ed70
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-05-24 14:19:41 +0900

    fix: select 선택하는 코드 위치 변경

--- cypress/e2e/3-write-page/write.spec.cy.ts
+++ cypress/e2e/3-write-page/write.spec.cy.ts
@@ -42,48 +42,11 @@ describe("write page e2e test", () => {
     });
   });
 
-  it("가계부에 1개의 수입 내역을 작성한다.", () => {
-    cy.wait(3000);
-    cy.get('[data-cy="write-input-container"]').within(() => {
-      cy.get("select").eq(0).select(2);
       cy.get('input[type="date"]').type("2023-05-22");
-      cy.get("select").eq(1).select(2);
-      cy.get("select").eq(2).select(2);
-      cy.get('input[type="text"]').eq(0).type("거래처 테스트");
-      cy.get('input[type="number"]').type("3000");
-      cy.get('input[type="text"]').eq(1).type("메모 테스트");
-    });
-    cy.get('[data-cy="write-submit-button"]').click();
-    cy.wait("@uploadBook");
-  });
-
-  it("가계부에 지출 내역을 작성하고 동일한 내용을 공유가계부에 추가한다.", () => {
-    cy.get('[data-cy="add-otherBook-button"]').click();
-    cy.get('[data-cy="other-account-book"]').find("button").click();
-    cy.get('[data-cy="modal-submit-button"]').click();
-    cy.get('[data-cy="write-input-container"]').within(() => {
-      cy.get("select").eq(0).select(2);
-      cy.get('input[type="date"]').type("2023-05-22");
-      cy.get("select").eq(1).select(2);
-      cy.get("select").eq(2).select(2);
-      cy.get('input[type="text"]').eq(0).type("거래처 테스트");
-      cy.get('input[type="number"]').type("3000");
-      cy.get('input[type="text"]').eq(1).type("메모 테스트");
-    });
-    cy.get('[data-cy="write-submit-button"]').click();
-    cy.wait("@uploadBook");
-  });
-
   it("가계부에 2개 이상의 내역을 작성한다.", () => {
-    // 내역 칸 하나 추가
-    cy.get('[data-cy="write-plus-button"]').click();
-
     // 첫번째 내역 작성
-    cy.get('[data-cy="write-input-container"]')
-      .eq(0)
-      .get("select")
-      .eq(0)
-      .select(2);
+    cy.wait(3000);
+
     cy.get('[data-cy="write-input-container"]')
       .eq(0)
       .find('input[type="date"]')
@@ -113,7 +76,15 @@ describe("write page e2e test", () => {
       .eq(1)
       .type("첫번째 내역");
 
+    // 내역 칸 하나 추가
+    cy.get('[data-cy="write-plus-button"]').click();
+
     // 두번째 내역 작성
+    cy.get('[data-cy="write-input-container"]')
+      .eq(0)
+      .find("select")
+      .eq(0)
+      .select(2);
     cy.get('[data-cy="write-input-container"]')
       .eq(1)
       .find("select")
@@ -151,6 +122,39 @@ describe("write page e2e test", () => {
     cy.contains("작성 완료").click();
     cy.wait("@uploadBook");
   });
+
+  it("가계부에 1개의 수입 내역을 작성한다.", () => {
+    cy.wait(3000);
+    cy.get('[data-cy="write-input-container"]').within(() => {
+      cy.get("select").eq(0).select(2);
+      cy.get('input[type="date"]').type("2023-05-22");
+      cy.get("select").eq(1).select(2);
+      cy.get("select").eq(2).select(2);
+      cy.get('input[type="text"]').eq(0).type("거래처 테스트");
+      cy.get('input[type="number"]').type("3000");
+      cy.get('input[type="text"]').eq(1).type("메모 테스트");
+    });
+    cy.get('[data-cy="write-submit-button"]').click();
+    cy.wait("@uploadBook");
+  });
+
+  it("가계부에 지출 내역을 작성하고 동일한 내용을 공유가계부에 추가한다.", () => {
+    cy.get('[data-cy="add-otherBook-button"]').click();
+    cy.get('[data-cy="other-account-book"]').find("button").click();
+    cy.get('[data-cy="modal-submit-button"]').click();
+    cy.get('[data-cy="write-input-container"]').within(() => {
+      cy.get("select").eq(0).select(2);
+      cy.get('input[type="date"]').type("2023-05-22");
+      cy.get("select").eq(1).select(2);
+      cy.get("select").eq(2).select(2);
+      cy.get('input[type="text"]').eq(0).type("거래처 테스트");
+      cy.get('input[type="number"]').type("3000");
+      cy.get('input[type="text"]').eq(1).type("메모 테스트");
+    });
+    cy.get('[data-cy="write-submit-button"]').click();
+    cy.wait("@uploadBook");
+  });
+
   it("가계부 내역 작성 시 빈 칸이 있어 가계부 작성에 실패한다.", () => {
     cy.wait(3000);
     cy.get('[data-cy="write-input-container"]').within(() => {
