895c3a6e4766343ed20d869813d92cf15d696fc5
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-05-30 19:55:01 +0900

    test: 가계부 정보 수정, 카테고리 삭제 시나리오 테스트 코드 추가

---
+++ cypress/e2e/7-setting-page/accountBook.spec.cy.ts
@@ -0,0 +1,44 @@
+import API from "../../../src/api/config";
+
+describe("accountBook setting page e2e test", () => {
+  beforeEach(() => {
+    cy.login().then(() => {
+      cy.visit(
+        `http://localhost:3000/setting/book/${localStorage.getItem(
+          "privateAccountBookId",
+        )}`,
+      );
+      cy.intercept("PATCH", `${API.ACCOUNT_BOOK}/53`, {
+        statusCode: 200,
+      }).as("editAccountBook");
+    });
+  });
+  it("가계부 정보를 수정한다.", () => {
+    cy.get('[data-cy="dot-button"]').eq(0).click();
+    cy.contains("수정하기").click();
+    cy.get('[data-cy="book-setting-container"]')
+      .eq(0)
+      .within(() => {
+        cy.get('input[type="text"]').eq(0).clear();
+        cy.get('input[type="text"]').eq(0).type("관계 테스트");
+        cy.get('input[type="text"]').eq(1).type("새로운 카테고리 테스트");
+        cy.get('[data-cy="add-category-button"]').click();
+        cy.get('[data-cy="avatar-color"]').eq(3).click();
+        cy.get('[data-cy="edit-submit-button"]').click();
+        cy.wait("@editAccountBook");
+      });
+  });
+  it("현재 존재하는 카테고리를 삭제한다.", () => {
+    cy.get('[data-cy="dot-button"]').eq(0).click();
+    cy.contains("수정하기").click();
+    cy.get('[data-cy="book-setting-container"]')
+      .eq(0)
+      .within(() => {
+        cy.get('[data-cy="default-category"]').eq(0).find("button").click();
+      });
+    cy.contains("확인").click();
+    cy.get('[data-cy="edit-submit-button"]').eq(0).click();
+    cy.wait("@editAccountBook");
+  });
+  it("기존 가계부에서 새로운 사용자를 추가한다.", () => {});
+});
