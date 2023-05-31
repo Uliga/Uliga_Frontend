7fe3cb1c9951d5ed131aafc34decdca0b54b6032
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-05-31 17:54:42 +0900

    design: 내 정보 페이지 스크롤 가능한 컨테이너로 변경

--- src/pages/setting/me/index.tsx
+++ src/pages/setting/me/index.tsx
@@ -10,7 +10,7 @@ import IconButton from "../../../components/IconButton";
 
 const Container = styled.div`
   width: 88rem;
-  height: 83rem;
+  height: 57rem;
   border: 0.1rem solid ${COLORS.GREY[200]};
   border-radius: 0.5rem;
   padding: 4rem;
@@ -22,6 +22,11 @@ const Container = styled.div`
   }
   margin-left: 27.5rem;
   position: relative;
+  overflow: scroll;
+  overflow-x: hidden;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 const Info = styled.div`
   display: flex;
@@ -29,19 +34,15 @@ const Info = styled.div`
   gap: 3rem;
 `;
 
-const InfoInput = styled(Input)``;
 const ModifyButton = styled(Button)`
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  margin: 0 4rem 4rem 0;
   border-radius: 0.5rem;
-  padding: 1rem 3rem 1rem 3rem;
-  font-size: 1.3rem;
+  padding: 1rem 3.5rem;
+  font-size: 1.4rem;
+  position: absolute;
+  right: 0;
+  bottom: 0;
 `;
 const DeleteButton = styled(Button)`
-  position: absolute;
-  bottom: 0;
   margin: 0 0 4rem 0;
   padding: 0;
   color: ${COLORS.GREY[400]};
@@ -66,6 +67,13 @@ const Password = styled.div`
     margin-right: 1.5rem;
     right: 0;
   }
+`;
+
+const Bottom = styled.div`
+  width: 100%;
+  height: 10rem;
+  position: relative;
+  padding-top: 2rem;
 `;
 
 export default function SettingMe() {
@@ -115,7 +123,7 @@ export default function SettingMe() {
           input.label === "비밀번호" ||
           input.label === "비밀번호 확인" ? (
             <Password>
-              <InfoInput
+              <Input
                 key={input.label}
                 size={80}
                 label={input.label}
@@ -131,7 +139,7 @@ export default function SettingMe() {
               />
             </Password>
           ) : (
-            <InfoInput
+            <Input
               key={input.label}
               size={80}
               label={input.label}
@@ -144,22 +152,24 @@ export default function SettingMe() {
           ),
         )}
       </Info>
-      <ModifyButton
-        theme="quaternary"
-        title="수정하기"
-        onClick={checkNickname}
-        disabled={
-          (password.length > 0 || passwordCheck.length > 0) &&
-          !(isValidatePassword && password === passwordCheck)
-        }
-      />
-      <DeleteButton
-        theme="tertiary"
-        title="회원 탈퇴"
-        onClick={() => {
-          setDeleteDialogOpen(true);
-        }}
-      />
+      <Bottom>
+        <ModifyButton
+          theme="quaternary"
+          title="수정하기"
+          onClick={checkNickname}
+          disabled={
+            (password.length > 0 || passwordCheck.length > 0) &&
+            !(isValidatePassword && password === passwordCheck)
+          }
+        />
+        <DeleteButton
+          theme="tertiary"
+          title="회원 탈퇴"
+          onClick={() => {
+            setDeleteDialogOpen(true);
+          }}
+        />
+      </Bottom>
     </Container>
   );
 }
