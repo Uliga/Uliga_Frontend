ef3f8201fcbd95c709dfd98663bfbe71a2491ebb
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2023-06-28 19:09:53 +0900

    chore: 일렉트론 초기화면 1920*1080으로 수정

--- public/main.js
+++ public/main.js
@@ -8,8 +8,8 @@ remote.initialize();
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
